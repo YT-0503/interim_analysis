3651ad523df8f0d49334622ea2d03d043dc72fea
YT-0503 <130898540+YT-0503@users.noreply.github.com>
2023-06-08 18:42:28 +0900

    Update of_pocock_alpha_spending.R

--- interim_analysis/of_pocock_alpha_spending.R
+++ interim_analysis/of_pocock_alpha_spending.R
@@ -32,7 +32,7 @@ for (i in 1:n_analyses) {
 # Calculate alpha differences
 pocock_alpha_differences <- c(pocock_alpha_values[1], diff(pocock_alpha_values))
 of_alpha_differences <- c(of_alpha_values[1], diff(of_alpha_values))
-pocock_of_alpha_differences <- c(pocock_alpha_differences[1] - of_alpha_differences[1], diff(pocock_alpha_differences - of_alpha_differences))
+pocock_of_alpha_differences <- c(pocock_alpha_values[1] - of_alpha_values[1], diff(pocock_alpha_values - of_alpha_values))
 
 # Display alpha values and differences for each analysis
 for (i in 1:n_analyses){
